0156bdc117721645eac14601bd25f572a3a37f01
adsf117 <@Thecats.05>
2021-02-28 01:44:18 -0500

    data base  integration

--- app/src/main/java/com/puzzlebench/kitsu_aac/data/local/AnimeDao.kt
+++ app/src/main/java/com/puzzlebench/kitsu_aac/data/local/AnimeDao.kt
@@ -1,4 +1,12 @@
 package com.puzzlebench.kitsu_aac.data.local
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
+@Dao
 interface AnimeDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(event: AnimeEntity): Long
 }
